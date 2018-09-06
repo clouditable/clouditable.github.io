84ec1117397fd25faa51dddd94898b90a6a662a9
buluta3 <edabuluta3@gmail.com>
2018-09-06 16:13:43 +0300

    x

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from './components/header/header';
 import About from './components/about/about';
 import Resume from './components/resume/resume';
 import Portfolio from './components/portfolio/portfolio';
-import Testimonials from  './components/testimonials/testimonials';
+import Testimonials from './components/testimonials/testimonials';
 import ContactUs from './components/contactus/contactus';
 import Footer from './components/footer/footer';
 import resumeData from './resumeData';
@@ -12,13 +12,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-       <Route exact path={} render={ (routerProps) => <Header resumeData={resumeData}/> } />
-       <Route exact path={} render={ (routerProps) => <About resumeData={resumeData}/> } />
-       <Route exact path={} render={ (routerProps) => <Resume resumeData={resumeData}/> } />
-       <Route exact path={} render={ (routerProps) => <Portfolio resumeData={resumeData}/> } />
-       <Route exact path={} render={ (routerProps) => <Testimonials resumeData={resumeData}/> } />
-       <Route exact path={} render={ (routerProps) => <ContactUs resumeData={resumeData}/> } />
-       <Route exact path={} render={ (routerProps) => <Footer resumeData={resumeData}/> } />
+        <Header resumeData={resumeData} />
+        <About resumeData={resumeData} />
+        <Resume resumeData={resumeData} />
+        <Portfolio resumeData={resumeData} />
+        <Testimonials resumeData={resumeData} />
+        <ContactUs resumeData={resumeData} />
+        <Footer resumeData={resumeData} />
       </div>
     );
   }
