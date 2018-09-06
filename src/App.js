697b59a94ecf3c538a139fdb2fe89a3a7c1050c9
buluta3 <edabuluta3@gmail.com>
2018-09-06 15:51:09 +0300

    router removed

--- src/App.js
+++ src/App.js
@@ -7,19 +7,18 @@ import Testimonials from  './components/testimonials/testimonials';
 import ContactUs from './components/contactus/contactus';
 import Footer from './components/footer/footer';
 import resumeData from './resumeData';
-import { Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       <Route exact path={`/header`} render={ (routerProps) => <Header resumeData={resumeData} routerProps={routerProps} /> } />
-       <Route exact path={`/about`} render={ (routerProps) => <About resumeData={resumeData} routerProps={routerProps} /> } />
-       <Route exact path={`/resume`} render={ (routerProps) => <Resume resumeData={resumeData} routerProps={routerProps} /> } />
-       <Route exact path={`/portfolio`} render={ (routerProps) => <Portfolio resumeData={resumeData} routerProps={routerProps} /> } />
-       <Route exact path={`/testimonials`} render={ (routerProps) => <Testimonials resumeData={resumeData} routerProps={routerProps} /> } />
-       <Route exact path={`/contactUs`} render={ (routerProps) => <ContactUs resumeData={resumeData} routerProps={routerProps} /> } />
-       <Route exact path={`/footer`} render={ (routerProps) => <Footer resumeData={resumeData} routerProps={routerProps} /> } />
+       <Route exact path={} render={ (routerProps) => <Header resumeData={resumeData}/> } />
+       <Route exact path={} render={ (routerProps) => <About resumeData={resumeData}/> } />
+       <Route exact path={} render={ (routerProps) => <Resume resumeData={resumeData}/> } />
+       <Route exact path={} render={ (routerProps) => <Portfolio resumeData={resumeData}/> } />
+       <Route exact path={} render={ (routerProps) => <Testimonials resumeData={resumeData}/> } />
+       <Route exact path={} render={ (routerProps) => <ContactUs resumeData={resumeData}/> } />
+       <Route exact path={} render={ (routerProps) => <Footer resumeData={resumeData}/> } />
       </div>
     );
   }
