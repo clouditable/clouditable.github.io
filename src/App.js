2f8e7f0c70e061536e9c9f058e8622c7e710b459
buluta3 <edabuluta3@gmail.com>
2018-09-06 15:49:16 +0300

    updated

--- src/App.js
+++ src/App.js
@@ -13,13 +13,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-       <Route exact path={} render={ (routerProps) => <Header resumeData={resumeData}/> } />
-       <Route exact path={} render={ (routerProps) => <About resumeData={resumeData}/> } />
-       <Route exact path={} render={ (routerProps) => <Resume resumeData={resumeData}/> } />
-       <Route exact path={} render={ (routerProps) => <Portfolio resumeData={resumeData}/> } />
-       <Route exact path={} render={ (routerProps) => <Testimonials resumeData={resumeData}/> } />
-       <Route exact path={} render={ (routerProps) => <ContactUs resumeData={resumeData}/> } />
-       <Route exact path={} render={ (routerProps) => <Footer resumeData={resumeData}/> } />
+       <Route exact path={`/header`} render={ (routerProps) => <Header resumeData={resumeData} routerProps={routerProps} /> } />
+       <Route exact path={`/about`} render={ (routerProps) => <About resumeData={resumeData} routerProps={routerProps} /> } />
+       <Route exact path={`/resume`} render={ (routerProps) => <Resume resumeData={resumeData} routerProps={routerProps} /> } />
+       <Route exact path={`/portfolio`} render={ (routerProps) => <Portfolio resumeData={resumeData} routerProps={routerProps} /> } />
+       <Route exact path={`/testimonials`} render={ (routerProps) => <Testimonials resumeData={resumeData} routerProps={routerProps} /> } />
+       <Route exact path={`/contactUs`} render={ (routerProps) => <ContactUs resumeData={resumeData} routerProps={routerProps} /> } />
+       <Route exact path={`/footer`} render={ (routerProps) => <Footer resumeData={resumeData} routerProps={routerProps} /> } />
       </div>
     );
   }
