ba747a7c60dce512dc79b837fee83a38d6466c9e
buluta3 <edabuluta3@gmail.com>
2018-09-06 16:37:38 +0300

    updated

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,119 +1,119 @@
 let resumeData = {
-  "imagebaseurl":"https://techonoeticsclub.github.io/",
-  "name": "Saurabh Mhatre",
-  "role": "Frontend Developer",
-  "emailid": "Your emailid",
-  "skypeid": "Your skypeid",
+  "imagebaseurl": "https://techonoeticsclub.github.io/",
+  "name": "Eda Bulut Ağırman",
+  "role": "Full Stack Web Developer",
+  "emailid": "edabuluta3@gmail.com",
+  "skypeid": "edabuluta3",
   "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
-  "socialLinks":[
-      {
-        "name":"facebook",
-        "url":"http://facebook.com/",
-        "className":"fa fa-facebook"
-      },
-      {
-        "name":"twitter",
-        "url":"http://twitter.com",
-        "className":"fa fa-twitter"
-      },
-      {
-        "name":"google-plus",
-        "url":"http://googleplus.com/",
-        "className":"fa fa-google-plus"
-      },
-      {
-        "name":"linkedin",
-        "url":"https://www.linkedin.com/",
-        "className":"fa fa-linkedin"
-      },
-      {
-        "name":"github",
-        "url":"http://github.com/",
-        "className":"fa fa-github"
-      },
-      {
-        "name":"skype",
-        "url":"http://skype.com",
-        "className":"fa fa-skype"
-      }
-    ],
-  "aboutme":"A little info about me",
-  "address":"Somewhere in this world",
-  "website":"http://technoetics.in",
-  "education":[
+  "socialLinks": [
     {
-      "UniversityName":"Some University",
-      "specialization":"Some specialization",
-      "MonthOfPassing":"Jan",
-      "YearOfPassing":"2018",
-      "Achievements":"Some Achievements"
+      "name": "facebook",
+      "url": "http://facebook.com/clouditable",
+      "className": "fa fa-facebook"
     },
     {
-      "UniversityName":"Some University",
-      "specialization":"Some specialization",
-      "MonthOfPassing":"Jan",
-      "YearOfPassing":"2018",
-      "Achievements":"Some Achievements"
+      "name": "twitter",
+      "url": "http://twitter.com/clouditable",
+      "className": "fa fa-twitter"
+    },
+    {
+      "name": "google-plus",
+      "url": "http://googleplus.com/112956102848539252899",
+      "className": "fa fa-google-plus"
+    },
+    {
+      "name": "linkedin",
+      "url": "https://www.linkedin.com/in/eda-bulut-agirman-a22a1510a/",
+      "className": "fa fa-linkedin"
+    },
+    {
+      "name": "github",
+      "url": "http://github.com/clouditable",
+      "className": "fa fa-github"
     }
   ],
-  "work":[
+  "aboutme": "A little info about me",
+  "address": "Ankara, Turkey",
+  "website": "edabuluta3@gmail.com",
+  "education": [
     {
-      "CompanyName":"Some Company",
-      "specialization":"Some specialization",
-      "MonthOfLeaving":"Jan",
-      "YearOfLeaving":"2018",
-      "Achievements":"Some Achievements"
-    },
-    {
-      "CompanyName":"Some Company",
-      "specialization":"Some specialization",
-      "MonthOfLeaving":"Jan",
-      "YearOfLeaving":"2018",
-      "Achievements":"Some Achievements"
+      "UniversityName": "Computer Programming",
+      "specialization": "Web Programming",
+      "MonthOfPassing": "Sep",
+      "YearOfPassing": "2018"
     }
   ],
-  "skillsDescription":"Your skills here",
-  "skills":[
+  "work": [
     {
-      "skillname":"HTML5"
+      "CompanyName": "Global Group Education",
+      "specialization": "Full Stack Web Developer",
+      "MonthOfLeaving": "May",
+      "YearOfLeaving": "2014"
     },
     {
-      "skillname":"CSS"
+      "CompanyName": "Global Group Software",
+      "specialization": "Full Stack Web Developer",
+      "MonthOfLeaving": "July",
+      "YearOfLeaving": "2018",
     },
     {
-      "skillname":"Reactjs"
+      "CompanyName": "Freelancer",
+      "specialization": "MERN Stack Web Developer",
+      "MonthOfLeaving": "Sep",
+      "YearOfLeaving": "2014",
     }
   ],
-  "portfolio":[
+  "skillsDescription": "Your skills here",
+  "skills": [
     {
-      "name":"project1",
-      "description":"mobileapp",
-      "imgurl":"images/portfolio/phone.jpg"
+      "skillname": "ReactJS"
     },
     {
-      "name":"project2",
-      "description":"mobileapp",
-      "imgurl":"images/portfolio/project.jpg"
+      "skillname": "NodeJS"
     },
     {
-      "name":"project3",
-      "description":"mobileapp",
-      "imgurl":"images/portfolio/project2.png"
+      "skillname": "ExpressJS"
     },
     {
-      "name":"project4",
-      "description":"mobileapp",
-      "imgurl":"images/portfolio/phone.jpg"
+      "skillname": "MongoDB"
+    },
+    {
+      "skillname": "HTML"
+    },
+    {
+      "skillname": "CSS"
     }
   ],
-  "testimonials":[
+  "portfolio": [
     {
-      "description":"This is a sample testimonial",
-      "name":"Some technical guy"
+      "name": "project1",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/phone.jpg"
     },
     {
-      "description":"This is a sample testimonial",
-      "name":"Some technical guy"
+      "name": "project2",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/project.jpg"
+    },
+    {
+      "name": "project3",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/project2.png"
+    },
+    {
+      "name": "project4",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/phone.jpg"
+    }
+  ],
+  "testimonials": [
+    {
+      "description": "This is a sample testimonial",
+      "name": "Some technical guy"
+    },
+    {
+      "description": "This is a sample testimonial",
+      "name": "Some technical guy"
     }
   ]
 }
